2aabe53742bdb15dcb5016aed4d65bb7ca520724
Yunru Wang <yunru.wang@ifi.lmu.de>
2024-12-06 14:11:27 +0100

    fix slight problems

--- src/Main.py
+++ src/Main.py
@@ -36,7 +36,7 @@ def main(Args, FeatureOption):
         GetApkData(NCpuCores, *apk_paths)
 
 
-    RandomClassification(dual, penalty, train_years, True, os.path.join(dir, "training", "malware"), os.path.join(dir, "training", "goodware"), TestSize, FeatureOption, Model, NumFeatForExp, os.path.join(dir, "training"),  saveTrainSet="", enableFuture=True, futureYears=["2021", "2022"], futureMalwareCorpus=os.path.join(dir, "test", "malware"), futureGoodwareCorpus=s.path.join(dir, "test", "goodware"))
+    RandomClassification(dual, penalty, train_years, True, os.path.join(dir, "training", "malware"), os.path.join(dir, "training", "goodware"), TestSize, FeatureOption, Model, NumFeatForExp, os.path.join(dir, "training"),  saveTrainSet="", enableFuture=True, futureYears=["2021", "2022"], futureMalwareCorpus=os.path.join(dir, "test", "malware"), futureGoodwareCorpus=os.path.join(dir, "test", "goodware"))
  
     HoldoutClassification(dual, penalty, ["2021", "2022"], os.path.join(dir, "training"), True, os.path.join(dir, "test", "malware"), os.path.join(dir, "test", "goodware"), TestSize, FeatureOption, Model, NumFeatForExp)
     log_file.close()
