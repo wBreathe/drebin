d2f7d1e7ea47d9af8dc8b1d57ec9fe549a6b87a7
YunruWang <yunru.wang@ifi.lmu.de>
2025-01-20 00:27:49 +0100

    fix save

--- theoretical_bound.py
+++ theoretical_bound.py
@@ -3,6 +3,7 @@ import pickle
 import numpy as np
 import math
 import csv
+import re
 def error_bound(
         train_loss: float,
         m: int,
@@ -15,21 +16,27 @@ def error_bound(
 
 
 if __name__ == "__main__":
+    samples={"0.1": 35277, "0.2": 34208, "0.3": 26326, "0.4":18225, "0.5": 13018}
+    for file_name in os.listdir("./temp/"):
+        with open(os.path.join("temp", file_name), "rb") as f:
+            record = pickle.load(f)
+        
+        match = re.search(r'testSize-([\d\.]+)', file_name)
+        if match:
+            test_size = match.group(1)
+        print("testSize:", test_size)
+        total_samples = samples[test_size] 
+        delta = 0.05
+        bounds = [("norm_diff", "train_loss", "test_loss", "bound")]
+        for _, v in record.items():
+            assert("full" in v.keys() and "train_loss" in v.keys() and "test_loss" in v.keys())
+            norm_diff = v["full"][0]
+            train_loss = v["train_loss"][0]
+            test_loss = v["test_loss"][0]
+            bounds.append((norm_diff, train_loss, test_loss, error_bound(train_loss, total_samples, delta, norm_diff)))
     
-    with open("temp/num-100_kernel-linear_testSize-0.2_priorPortion-0.2_stats_1737312084", "rb") as f:
-        record = pickle.load(f)
-    total_samples = 34208 
-    delta = 0.05
-    bounds = [("norm_diff", "train_loss", "test_loss", "bound")]
-    for _, v in record.items():
-        assert("full" in v.keys() and "train_loss" in v.keys() and "test_loss" in v.keys())
-        norm_diff = v["full"][0]
-        train_loss = v["train_loss"][0]
-        test_loss = v["test_loss"][0]
-        bounds.append((norm_diff, train_loss, test_loss, error_bound(train_loss, total_samples, delta, norm_diff)))
-    
-    csv_file = "output_0.2.csv"
+        csv_file = f"{file_name}.csv"
 
-    with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerows(bounds)
+        with open(os.path.join("results", csv_file), mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerows(bounds)
